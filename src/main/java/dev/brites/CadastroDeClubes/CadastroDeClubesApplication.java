3bdd6b0cb7d1411db5de8f1b404dcf7f83eb2d78
Nicholas Brites <nickbrites1@gmail.com>
2025-10-03 10:56:16 -0300

    first commit

---
+++ src/main/java/dev/brites/CadastroDeClubes/CadastroDeClubesApplication.java
@@ -0,0 +1,13 @@
+package dev.brites.CadastroDeClubes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeClubesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeClubesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/brites/CadastroDeClubes/CadastroDeClubesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brites.CadastroDeClubes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeClubesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
